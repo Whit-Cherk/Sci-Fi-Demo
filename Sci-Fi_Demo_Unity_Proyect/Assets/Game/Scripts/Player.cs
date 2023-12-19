c16a2ebb2b4eba41bb41ead0ec6ce26b2f8bf9ec
Whit-Cherk <kevinsebestien@gmail.com>
2023-12-18 21:50:18 -0400

    Player and movement
    Implemented object "Player" and basic movement. Charachter Controller is amazing.

---
+++ Sci-Fi_Demo_Unity_Proyect/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{ 
+    // Referencias
+    private CharacterController _controller; // Character Controller of the player. 
+
+    // Componentes
+    [SerializeField] private float _speed = 3.5f; // Speed variable for movement
+    private float _gravity = 9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); //Input en el eje X
+        float verticalInput = Input.GetAxis("Vertical"); //Input en el eje Z
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput); // Getting the position via user input
+        Vector3 velocity = direction * _speed; // summarized formula for use below
+        velocity.y -= _gravity;
+        _controller.Move(Time.deltaTime * velocity);
+    }
+}
